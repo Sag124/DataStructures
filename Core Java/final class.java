36fb5be21282eadbd24ff9fa35e10fa893225cc7
sagar597 <raghavendra.sagar@matrixintel.com>
2020-09-17 11:33:03 +0530

    Create final class.java

---
+++ Core Java/final class.java
@@ -0,0 +1,21 @@
+final class Test
+{
+    public final void show()
+    {
+        System.out.println("Hello");
+    }
+}
+
+class Test1 extends Test
+{
+    // cannot inherit/extended final classes.
+}
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+}
