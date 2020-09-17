b13d34ec65f8d968fefa4d17e6308d60dec3ed50
sagar597 <raghavendra.sagar@matrixintel.com>
2020-09-17 11:33:16 +0530

    Create final members.java

---
+++ Core Java/final members.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    
+    final int x;
+    
+    public Main()
+    {
+        x = 5; // Method 2 via constructor
+    }
+    
+    {
+        x = 5; // Method 3 via blocks
+    }
+    
+    
+    public static void main(String[] args) {
+        final int x;
+        x = 10; //Method 1 of initialisation
+        System.out.println(x);
+        
+    }
+}
